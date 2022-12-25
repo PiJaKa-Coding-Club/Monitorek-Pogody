7dd79de529727bafac0d34b19e458ad88f1a838f
Jakub Radzik <jradzik4@gmail.com>
2022-12-25 18:44:01 +0100

    init project

---
+++ frontend/src/styles/colors/colors.ts
@@ -0,0 +1,18 @@
+export const weatherColors = {
+    sunny: '#FFFF88',
+    partialySunny: '#F9F5DE',
+    cloudy: '#E6E6E6',
+    rainy: '#E6E6E6',
+    stormy: '#B3B3B3',
+    snowy: '#F4F2FF',
+    windy: '#E6E6E6',
+    tornado: '#E6E6E6',
+};
+
+export const black = '#000000';
+export const white = '#FFFFFF';
+
+export const header = '#DAE8FC';
+export const background = '#F4F2FF';
+export const button = '#CCE5FF';
+export const badge = '#FFFF88';
---
+++ frontend/src/styles/fonts/index.ts
@@ -0,0 +1,1 @@
+export const mainFont = 'Courier New, Courier, monospace';
